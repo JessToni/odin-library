1c55dc81373ea0ac425a6f3ba2451175f274215d
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-09-18 05:55:33 +0800

    Add Book object contsructor

---
+++ library.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title, author, pages, genre, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.genre = genre;
+  this.status = status;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
