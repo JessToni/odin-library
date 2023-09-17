3aa2fc87802db895c2dca6e3b1be3dd58344746b
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-09-18 06:14:32 +0800

    Load JS code after DOM content has loaded

--- library.js
+++ library.js
@@ -1,15 +1,17 @@
-const myLibrary = [];
+document.addEventListener("DOMContentLoaded", function(event) {
+    const myLibrary = [];
 
-function Book(title, author, pages, genre, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.genre = genre;
-  this.status = status;
-}
+    function Book(title, author, pages, genre, status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.genre = genre;
+        this.status = status;
+    }
 
-function addBookToLibrary(obj) {
-  myLibrary.push(obj);
-}
+    function addBookToLibrary(obj) {
+        myLibrary.push(obj);
+    }
 
-const book1 = new Book("Harry Potter", "Donald", 254, "Punk/Rock", "Thrown Away");
+    const book1 = new Book("Harry Potter", "Donald", 254, "Punk/Rock", "Thrown Away");
+});
