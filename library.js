e9faa3dbe327c7c2ff648621729f8e8462631f70
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-09-18 06:36:08 +0800

    Place array and functions outside of eventListener

--- library.js
+++ library.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const myLibrary = [];
+const myLibrary = [];
 
-    function Book(title, author, pages, genre, status) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.genre = genre;
-        this.status = status;
-    }
+function Book(title, author, pages, genre, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.genre = genre;
+    this.status = status;
+}
 
-    function addBookToLibrary(obj) {
-        myLibrary.push(obj);
-    }
-
+document.addEventListener("DOMContentLoaded", function() {
     const book1 = new Book("Harry Potter", "Donald", 254, "Punk/Rock", "Thrown Away");
+    addBookToLibrary(book1);
+    console.log(myLibrary);
+    console.log(book1);
 });
+
+function addBookToLibrary(obj) {
+    myLibrary.push(obj);
+}
